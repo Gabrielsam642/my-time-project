ea8daadcbdb2f1c67f1286228c4093c7324f40c5
Gabriel Samuel <samuelgab246@gmail.com>
2024-01-16 17:16:17 -0800

    time

---
+++ TIME PROJECT.js
@@ -0,0 +1,39 @@
+// alert ("alll")
+let sam = 0;
+let uel =document.getElementById("num")
+console.log(uel) 
+
+function added(){
+    sam += 1
+    uel.innerText = sam
+}
+
+function added1(){
+    sam += 2
+    uel.innerText = sam
+}
+
+function added2(){
+    sam += 3
+    uel.innerText = sam
+}
+
+
+let samu = 0;
+let uelu =document.getElementById("unts")
+console.log(uelu) 
+
+function added3(){
+    samu += 1
+    uelu.innerText = samu
+}
+
+function added4(){
+    samu += 2
+    uelu.innerText = samu
+}
+
+function added5(){
+    samu += 3
+    uelu.innerText = samu
+}
